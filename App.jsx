3da9d83dfa39a7c36a12d176adc2d871cc43f8df
ALITIMUR <54514668+ALITIMUR@users.noreply.github.com>
2025-06-16 16:36:06 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="p-8 text-center text-xl">
+      <h1>Welcome to Genesis Capital</h1>
+      <p>This is the starting point for your official site.</p>
+    </div>
+  );
+}
